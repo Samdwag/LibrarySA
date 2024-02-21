c53e94673b26f6d99516b2e4d768fefd9373be32
Sami Alzoubi <samialzoubi@lewisu.edu>
2024-02-20 20:03:18 -0600

    Add project files.

---
+++ LibrarySA/Holding.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySA
+{
+    internal abstract class Holding
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }   
+        public string Description { get; set; }
+        public bool IsCheckedOut { get; set; }
+
+        public Holding()
+        {
+            Id = 0;
+            Title = string.Empty;
+            Description = string.Empty;
+            IsCheckedOut = false;
+        }
+
+        public Holding(int id, string title, string description)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            IsCheckedOut = false;
+        }
+
+        public void Checkout()
+        {
+            IsCheckedOut = true;
+        }
+
+        public void CheckIn()
+        {
+            IsCheckedOut = false;
+        }
+
+        public abstract string HoldingType();
+        public override string ToString()
+        {
+            return $"{Id}\t{Title}\t{Description}\t{(IsCheckedOut ? "Yes" : "No")}";
+        }
+    }
+}
---
+++ LibrarySA/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Sami Alzoubi
+ * CPSC 23000
+ * February 20th, 2024
+ * Library
+ */
+
+
+
+
+using System;
+using static System.Console;
+
+
+
+namespace LibrarySA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("*******************************************************");
+            WriteLine("        LIBRARY MANAGEMENT SYSTEM VERSION 1.0");
+            WriteLine("*******************************************************");
+            WriteLine("This tool helps you manage a library's collections.");
+            WriteLine("Please use the menu to choose what you want to do.");
+            WriteLine();
+        }
+
+
+
+    }
+}
